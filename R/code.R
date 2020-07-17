8b7c99ce4bfd74458fc5734e01160881c14b8024
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2020-07-17 17:26:45 +0200

    rewrote ref with rlang

--- R/code.R
+++ R/code.R
@@ -1,42 +1,19 @@
+#' @importFrom rlang env caller_env enexpr is_symbol env_has as_string abort
 #' @export
 ref <- function(object) {
-  ref_env <- new.env(parent = parent.frame(n = 2))
-  class(ref_env) <- "ref_env"
-  name <- deparse(substitute(object))
-  ref_env$name <- name
-  ref_env
+  rfr <- env(caller_env())
+  class(rfr) <- "reference"
+  symbol <- enexpr(object)
+  if (!is_symbol(symbol) ||
+      !env_has(env = caller_env(), nms = as_string(symbol)))
+    abort("Object passed to the ref has to be a name present in calling environment!")
+  rfr$symbol <- symbol
+  rfr$name <- as_string(symbol)
+  rfr
 }
 
-replace_lang <- function(expr, item, replacement) {
-  if (length(expr) == 1) {
-    if (expr == item) replacement else expr
-  } else as.call(sapply(expr, function(elem) replace_lang(elem, item, replacement)))
-}
-
+#' @importFrom rlang eval_bare
 #' @export
-mod <- function(object, fun) {
-  UseMethod("mod")
-}
-
-#' @export
-mod.ref_env <- function(object, fun) {
-  if (missing(fun)) {
-    parent.env(object)[[object$name]]
-  } else {
-    expr <- str2lang(deparse(substitute(fun)))
-    argname <- str2lang(deparse(substitute(object)))
-    expr <- replace_lang(expr, argname, str2lang(object$name))
-    eval(expr, parent.env(object))
-  }
-}
-
-#' @export
-mod.default <- function(object, fun) {
-  if (missing(fun)) {
-    object
-  } else {
-    env <- list(object)
-    names(env) <- deparse(substitute(object))
-    eval(fun, env)
-  }
+val <- function(rfr) {
+  eval_bare(rfr$symbol, rfr)
 }
